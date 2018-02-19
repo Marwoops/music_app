c71f98467eb884c7125f9ead230b7d91e46d860e
Marwan Azizi <marwan.azizi@gmail.com>
2018-02-19 13:59:59 +0100

    fixed dimension of the app.

--- player/main.js
+++ player/main.js
@@ -1,3 +1,4 @@
+
 const electron = require('electron');
 const {app, BrowserWindow} = electron;
 
@@ -5,8 +6,11 @@ let mainWindow;
 
 app.on('ready', () => {
 
-    let mainWindow = new BrowserWindow({width: 800, height: 600});
+    let mainScreenDimensions = (((electron.screen).getPrimaryDisplay()).size);
 
+    //let mainWindow = new BrowserWindow({width: 550, height: 120, alwaysOnTop: true, x: mainScreenDimensions.width - 500, y: 0, autoHideMenuBar: true});
+    
+    let mainWindow = new BrowserWindow({width: (mainScreenDimensions.width / 4), height: (Math.round(mainScreenDimensions.height / 9)), alwaysOnTop: true, x: (mainScreenDimensions.width * 75 / 100), y: 0, autoHideMenuBar: true});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 });
